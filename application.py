cd8bb30816b51abfd8ef1bb57b4b4027973bc11d
U-AzureAD\DrDamirRaßloff <d.rassloff@accantec.com>
2024-10-07 10:32:57 +0200

    Reset of percentage bar

--- application.py
+++ application.py
@@ -214,6 +214,10 @@ def email_processing_thread(file_paths, sharepoint_site_url, list_name, user_ema
 @app.route('/', methods=['GET', 'POST'])
 def index():
     global progress_percentage
+    # Fortschritt und Statusmeldungen beim Neuladen der Seite zurücksetzen
+    if request.method == 'GET':
+        with lock:  # Thread-Safe Zurücksetzen
+            progress_percentage = 0
     if request.method == 'POST':
         # Überprüfe, ob die Datei im Request vorhanden ist
         if 'file' not in request.files:
