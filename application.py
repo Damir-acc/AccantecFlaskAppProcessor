2a30ec57d5db10590de4b8cac78467a6bb928f6d
U-AzureAD\DrDamirRaßloff <d.rassloff@accantec.com>
2024-10-08 13:32:16 +0200

    Change start run command to port:5000

--- application.py
+++ application.py
@@ -327,4 +327,4 @@ def check_complete():
         return jsonify({"completed": emails_completed}), 200
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=5000)
