7c3df21b83279f32da25c4cc99434841213fef6d
U-AzureAD\DrDamirRaßloff <d.rassloff@accantec.com>
2024-10-21 13:35:07 +0200

    First try to include 2FA into processor app

--- application.py
+++ application.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify, session
+from flask_session import Session
 import os
 import extract_msg
 import shutil
@@ -10,10 +11,71 @@ from werkzeug.utils import secure_filename
 import zipfile
 import threading
 import time
+import identity.web
+import requests
+
+import application_config
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads/'  # Verzeichnis für hochgeladene Dateien
 app.secret_key = 'supersecretkey'  # Für Flash-Nachrichten
+app.config.from_object(application_config)
+assert app.config["REDIRECT_PATH"] != "/", "REDIRECT_PATH must not be /"
+Session(app)
+
+app.jinja_env.globals.update(Auth=identity.web.Auth)  # Useful in template for B2C
+auth = identity.web.Auth(
+    session=session,
+    authority=app.config["AUTHORITY"],
+    client_id=app.config["CLIENT_ID"],
+    client_credential=app.config["CLIENT_SECRET"],
+)
+
+@app.route(application_config.REDIRECT_PATH)
+def auth_response():
+    result = auth.complete_log_in(request.args)
+    if "error" in result:
+        return render_template("auth_error.html", result=result)
+    return redirect(url_for("index"))
+
+@app.route("/login")
+def login():
+    return render_template("login.html", version='1.0', **auth.log_in(
+        scopes=application_config.SCOPE, # Have user consent to scopes during log-in
+        redirect_uri=url_for("auth_response", _external=True, _scheme="https"), # Optional. If present, this absolute URL must match your app's redirect_uri registered in Microsoft Entra admin center
+        prompt="select_account",  # Optional.
+        ))
+
+@app.route("/logout")
+def logout():
+    return redirect(auth.log_out(url_for("index", _external=True)))
+
+@app.route("/")
+def index():
+    if not (app.config["CLIENT_ID"] and app.config["CLIENT_SECRET"]):
+        return render_template('config_error.html')
+    if not auth.get_user():
+        return redirect(url_for("login"))
+    return render_template('index.html', user=auth.get_user(), version='1.0')
+
+@app.route("/call_downstream_api")
+def call_downstream_api():
+    token = auth.get_token_for_user(application_config.SCOPE)
+    if "error" in token:
+        return redirect(url_for("login"))
+    # Use access token to call downstream api
+    api_result = requests.get(
+        application_config.ENDPOINT,
+        headers={'Authorization': 'Bearer ' + token['access_token']},
+        timeout=30,
+    ).json()
+    return render_template('display.html', result=api_result)
+
+@app.route("/processor")
+def processor():
+    if not auth.get_user():
+        return redirect(url_for("login"))  # Weiterleitung zur Login-Seite, falls nicht eingeloggt
+    return render_template('processor.html')
 
 # Globale Variablen für Fortschritt und Status
 progress = 0
